e91fd7abac8e33c3125dbf7c4cf024f2c07a0b52
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-01-08 02:50:58 +0530

    cleaning-1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import openai
 import os
 import json
 import requests
+from rich import print, pretty
+pretty.install()
 from utils import *
 from gihutb_fetch import fetch_github_repo
 codebase_path=f'{os.path.dirname(os.path.realpath(__file__))}\\repo-downloads'
