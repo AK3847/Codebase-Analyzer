61c5e0070bfa9435ecb48e9cb6c8f8efd96d06b9
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-01-13 23:52:56 +0530

    added comments

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+#Modules
 import os
 import json
 import  shutil
@@ -7,19 +8,23 @@ import re
 import getpass
 import pwinput
 import tiktoken
-
 import openai
 from github import Github
-
 from rich import print, pretty
 pretty.install()
 
 
-
+#files witht this extensions are downloaded and sent for analyzing to ChatGPT
 allowed_extensions = ['c', 'h', 'cpp', 'hpp', 'java', 'py', 'html', 'css', 'js', 'php', 'ruby', 'swift', 'go', 'dart', 'kotlin', 'ts', 'tsx', 'csharp', 'vb', 'scala', 'r', 'rust', 'lua', 'shell', 'sh', 'bash', 'ps1', 'json', 'xml', 'yaml', 'toml', 'ini']
+
 # prompt_text="Analyze the following code and give suggestion for Code Improvement,Code Optimization,Bug Identification and Resolution, suggest no more than 2-3 points per topic.\n\n"
-repo_path=f"{os.path.dirname(os.path.realpath(__file__))}\\repo-downloads"
 prompt_text="Please review the code below and identify any syntax or logical errors, suggest ways to refactor and improve code quality, enhance performance, address security concerns, and bug identification & fix. Limit your response to 2-3 suggestion per field."
+
+#Directory path where the repository files are downloaded
+repo_path=f"{os.path.dirname(os.path.realpath(__file__))}\\repo-downloads"
+
+
+
 codebase_ASCII_ART=""" 
 
  ______  ______  _____   ______  ______  ______  ______  ______       ______  __   __  ______  __      __  __  ______  ______  ______    
